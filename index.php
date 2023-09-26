4f0a9dbabcece99f489f9ffcd15d443f8481abc7
19240569 <145827385+19240569@users.noreply.github.com>
2023-09-25 23:26:58 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Información sobre Ansible</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            color: #333;
+            background-color: #f9f9f9;
+            margin: 20px;
+        }
+
+        h1 {
+            color: #0056b3;
+        }
+
+        p {
+            line-height: 1.6;
+        }
+    </style>
+</head>
+<body>
+    <h1>¿Qué es Ansible?</h1>
+    <p>Ansible es una plataforma de automatización de TI de código abierto que facilita la automatización de tareas complejas de configuración, implementación y administración de sistemas. Utiliza un lenguaje de configuración simple basado en YAML y no requiere agentes en los sistemas remotos.</p>
+
+    <h1>Características principales de Ansible</h1>
+    <ul>
+        <li>Automatización sin agentes</li>
+        <li>Infraestructura como código (IaC)</li>
+        <li>Orquestación de múltiples servidores</li>
+        <li>Escalabilidad y flexibilidad</li>
+        <li>Integración con herramientas de gestión de configuración</li>
+    </ul>
+
+    <h1>Beneficios de usar Ansible</h1>
+    <ul>
+        <li>Simplicidad y facilidad de uso</li>
+        <li>Ahorro de tiempo y recursos</li>
+        <li>Consistencia en la configuración</li>
+        <li>Gestión centralizada</li>
+    </ul>
+
+    <h1>Recursos Adicionales</h1>
+    <p>Para obtener más información sobre Ansible, puedes consultar los siguientes recursos:</p>
+    <ul>
+        <li><a href="https://www.ansible.com/">Sitio web oficial de Ansible</a></li>
+        <li><a href="https://docs.ansible.com/">Documentación de Ansible</a></li>
+        <li><a href="https://github.com/ansible/ansible">Repositorio de GitHub de Ansible</a></li>
+    </ul>
+</body>
+</html>
